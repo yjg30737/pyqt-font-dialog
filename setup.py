a0cdee658db04f92a6292bff5623bc18148ec0a8
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-12 10:31:37 +0900

    Update setup.py to upload to pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-font-dialog',
-    version='0.1.0',
+    version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
     description='PyQt font dialog',
     url='https://github.com/yjg30737/pyqt-font-dialog.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8'
     ]
